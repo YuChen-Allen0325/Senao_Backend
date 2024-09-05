2f54fb2a81cb8166cd8824e288e1074a29920b56
allenxie <f0934015254@gmail.com>
2024-09-05 15:23:04 +0800

    add context manager

--- redirection/handler.py
+++ redirection/handler.py
@@ -6,9 +6,9 @@ def validate_url(url):
         if len(url) > 2048:
             raise Exception("URL too long")
 
-        response = requests.get(url)
-        if response.status_code != 200:
-            raise Exception("Invalid URL")
+        with requests.get(url) as response:
+            if response.status_code != 200:
+                raise Exception("Invalid URL")
     
     except Exception as e:
         raise e
